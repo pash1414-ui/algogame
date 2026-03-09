d73ccb2ad061884e02a8618175eb5ac153e899b1
pash1414-ui <pash1414@gmail.com>
2026-03-09 09:35:15 +0900

    Delete algo-game/algo-game directory

--- algo-game/algo-game/src/components/game/DeckArea.tsx
+++
@@ -1,4 +0,0 @@
-// 4人対戦では山札なし。このコンポーネントは使用しません。
-export default function DeckArea() {
-  return null;
-}
--- algo-game/algo-game/src/hooks/useGame.ts
+++
@@ -1,3 +0,0 @@
-'use client';
-import { useAlgoStore } from '@/store/gameStore';
-export { useAlgoStore as useGame };
